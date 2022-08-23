1900f49fc3480f796802e6c8d26ea21a68424158
山路倫子 <noriko@yamajitomokonoMacBook-ea.local>
2022-08-23 16:32:04 +0900

    home画面概ね完成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,9 @@ class HomeController < ApplicationController
   def index
     
   end
+
+  def search
+    @rooms = Room.where('room_address LIKE ?', "%#{params[:room_address]}%").where('room_introduction LIKE ? OR room_name LIKE ?', "%#{params[:keyword]}%", "%#{params[:keyword]}%")
+    @numbers = @rooms.count
+  end
 end
